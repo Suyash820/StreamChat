d3f606d485842552c454f6fad5848bf0cb2d307f
Suyash820 <you@example.com>
2026-06-04 18:07:43 +0530

    Fix auth cookies for production

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -6,11 +6,11 @@ export const generateToken = (userId, res) => {
   });
 
   res.cookie("jwt", token, {
-    maxAge: 7 * 24 * 60 * 60 * 1000, // MS
-    httpOnly: true, 
-    sameSite: "strict", 
-    secure: process.env.NODE_ENV !== "development",
-  });
+  maxAge: 7 * 24 * 60 * 60 * 1000,
+  httpOnly: true,
+  sameSite: "none",
+  secure: true,
+});
 
   return token;
 };
